18df591a37de00e2a6b8802f19a6c3991ffc398f
Rise Erpelding <rise@heysparkbox.com>
2021-04-09 17:01:37 -0400

    feat: add JavaScript
    -Copies previous JavaScript from Codepen (excluding the makeGrid function
    and its supporting functions). Includes adding all the functionality for
    getting values from the form and adding the generated colors to the DOM.
    
    -In order to call makeGrid in index.js, imports the function.
    
    -Adds a fix that wasn't needed in Codepen, coercing the gridSize
    variable to a number
    
    -Also removes a few unneeded console.logs
    
    FSA2021-104

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log('hello!!!!!')
+const { makeGrid } = require('../src/grid/grid');
+
+const form = document.getElementById('color-form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  clearGridContainer();
+  event.preventDefault();
+  const topLeft = addHash(document.getElementById('top-left').value);
+  const topRight = addHash(document.getElementById('top-right').value);
+  const bottomLeft = addHash(document.getElementById('bottom-left').value);
+  const bottomRight = addHash(document.getElementById('bottom-right').value);
+  const gridSize = Number(document.getElementById('grid-size').value);
+  const colorGrid = makeGrid(gridSize, topLeft, topRight, bottomLeft, bottomRight);
+  fillColors(colorGrid);
+}
+
+function clearGridContainer() {
+  const container = document.getElementById('grid-container');
+  container.innerHTML = '';
+}
+
+// probably some better validation for the hex values on this side of things would be nice too
+
+function addHash(str) {
+  return str.startsWith('#') ? str : '#' + str;
+}
+
+function fillColors(grid) {
+  const container = document.getElementById('grid-container');
+  grid.forEach((row) => {
+    const newRowDiv = document.createElement('div');
+    newRowDiv.classList.add('color-row');
+    container.appendChild(newRowDiv);
+    row.forEach((color, index) => {
+      const newColorDiv = document.createElement('div');
+      newRowDiv.appendChild(newColorDiv);
+      newColorDiv.style.backgroundColor = color;
+      newColorDiv.classList.add('color-tile');
+    });
+  });
+}
