753bb9fda1ad061960acf6e7f6b1433300670d4c
Rise Erpelding <rise.erpelding@gmail.com>
2025-09-05 19:17:43 -0500

    feat: support color mix mode

--- src/form/form-defaults.mjs
+++ src/form/form-defaults.mjs
@@ -6,12 +6,14 @@ function getFormDefaults() {
   // eslint-disable-next-line max-len
   const [topLeft, topRight, bottomLeft, bottomRight] = [colorInputs[0].value, colorInputs[1].value, colorInputs[2].value, colorInputs[3].value];
   const gridSize = document.querySelector('#grid-size').value;
+  const modeSelect = document.querySelector('#mode-select').value;
   const formDefaults = {
     topLeft,
     topRight,
     bottomLeft,
     bottomRight,
     gridSize,
+    modeSelect,
   };
   return formDefaults;
 }
@@ -20,6 +22,7 @@ function makeDefaultGrid() {
   const defaultFormValues = getFormDefaults();
   const {
     gridSize,
+    modeSelect,
     topLeft,
     topRight,
     bottomLeft,
@@ -27,6 +30,7 @@ function makeDefaultGrid() {
   } = defaultFormValues;
   const colorGrid = makeGrid(
     Number(gridSize),
+    modeSelect,
     topLeft,
     topRight,
     bottomLeft,
