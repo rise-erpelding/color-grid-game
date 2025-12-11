1625c6c950d8038573488ed5a0f34fa6f2cf66e0
Rise Erpelding <rise@heysparkbox.com>
2025-12-10 21:37:07 -0600

    test: update tests

--- src/grid/grid.test.js
+++ src/grid/grid.test.js
@@ -4,10 +4,30 @@ describe('makeGrid function', () => {
   test('makes a grid of colors', () => {
     const createdGrid = makeGrid(4, '#00ffff', '#7fff00', '#008b8b', '#006400');
     const expectedGrid = [
-      ['#00ffff', '#2affaa', '#55ff55', '#7fff00'],
-      ['#00d8d8', '#1cd490', '#39d048', '#55cb00'],
-      ['#00b2b2', '#0ea977', '#1ca03b', '#2a9800'],
-      ['#008b8b', '#007e5d', '#00712e', '#006400'],
+      [
+        'color-mix(in #00ffff, #7fff00 100%, #008b8b 0%)',
+        'color-mix(in #00ffff, #7fff00 67%, #008b8b 33%)',
+        'color-mix(in #00ffff, #7fff00 33%, #008b8b 67%)',
+        'color-mix(in #00ffff, #7fff00 0%, #008b8b 100%)',
+      ],
+      [
+        'color-mix(in #00ffff, color-mix(in #00ffff, #7fff00 100%, #008b8b 0%) 67%, color-mix(in #00ffff, #006400 100%, undefined 0%) 33%)',
+        'color-mix(in #00ffff, color-mix(in #00ffff, #7fff00 67%, #008b8b 33%) 67%, color-mix(in #00ffff, #006400 67%, undefined 33%) 33%)',
+        'color-mix(in #00ffff, color-mix(in #00ffff, #7fff00 33%, #008b8b 67%) 67%, color-mix(in #00ffff, #006400 33%, undefined 67%) 33%)',
+        'color-mix(in #00ffff, color-mix(in #00ffff, #7fff00 0%, #008b8b 100%) 67%, color-mix(in #00ffff, #006400 0%, undefined 100%) 33%)',
+      ],
+      [
+        'color-mix(in #00ffff, color-mix(in #00ffff, #7fff00 100%, #008b8b 0%) 33%, color-mix(in #00ffff, #006400 100%, undefined 0%) 67%)',
+        'color-mix(in #00ffff, color-mix(in #00ffff, #7fff00 67%, #008b8b 33%) 33%, color-mix(in #00ffff, #006400 67%, undefined 33%) 67%)',
+        'color-mix(in #00ffff, color-mix(in #00ffff, #7fff00 33%, #008b8b 67%) 33%, color-mix(in #00ffff, #006400 33%, undefined 67%) 67%)',
+        'color-mix(in #00ffff, color-mix(in #00ffff, #7fff00 0%, #008b8b 100%) 33%, color-mix(in #00ffff, #006400 0%, undefined 100%) 67%)',
+      ],
+      [
+        'color-mix(in #00ffff, #006400 100%, undefined 0%)',
+        'color-mix(in #00ffff, #006400 67%, undefined 33%)',
+        'color-mix(in #00ffff, #006400 33%, undefined 67%)',
+        'color-mix(in #00ffff, #006400 0%, undefined 100%)',
+      ],
     ];
     expect(createdGrid).toStrictEqual(expectedGrid);
   });
